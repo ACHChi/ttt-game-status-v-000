471d43c84b17df715812922c49a22c80d2437a51
Chao Hsuan Chi <allen0413@gmail.com>
2017-10-24 10:48:26 +0000

    automatically backed up by learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -20,9 +20,15 @@ def won?(board)
     win_index_1 = set[0]
     win_index_2 = set[1]
     win_index_3 = set[2]
-
+    
     position_1 = board[win_index_1]
     position_2 = board[win_index_2]
     position_3 = board[win_index_3]
-  }
+
+    if position_taken?(board, win_index_1)
+      if position_1 == position_2 ==position_3
+        return set[]
+      else
+      end
+    end
 end
