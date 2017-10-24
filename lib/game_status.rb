c9ab355ae82e0c8bfa53403db054e0a91b930eb2
Chao Hsuan Chi <allen0413@gmail.com>
2017-10-24 11:43:56 +0000

    automatically backed up by learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -25,12 +25,14 @@ def won?(board)
     position_2 = board[win_index_2]
     position_3 = board[win_index_3]
 
-    if position_taken?(board, win_index_1)
+    if position_taken?(board, set[0])
       if position_1 == position_2 && position_1 == position_3
         return set
       else
         return false
       end
+    else
+      return false
     end
   }
 end
